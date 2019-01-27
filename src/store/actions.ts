10ef3218a2709fff6355e67d557db631f2b56110
William Bain <bain.william.a@gmail.com>
2019-01-27 04:59:31 -0500

    Refactor: Add typing for actions

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,4 +1,13 @@
-export type Action = { type: string; payload: any }
+import { Direction } from '../constants'
+
+export type Action =
+    | { type: 'setDirection'; payload: { direction: Direction } }
+    | { type: 'start'; payload: {} }
+    | { type: 'pause'; payload: {} }
+    | { type: 'terminate'; payload: {} }
+    | { type: 'tick'; payload: { ticks: number } }
+    | { type: 'resumeGame'; payload: {} }
+    | { type: '__nonexhaustiveDontMatch'; [key: string]: any }
 
 export const SET_DIRECTION = 'setDirection'
 export const START = 'start'
