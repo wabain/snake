eb5f0def7816f54f32ef7c01f2a63658d47731c7
William Bain <bain.william.a@gmail.com>
2019-01-27 04:23:40 -0500

    Refactor: Add typechecking to index file

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
-import reducer from './store'
+import reducer from './store/index'
 import App from './components/app'
 
 const store = createStore(reducer)
--- webpack.conf.js
+++ webpack.conf.js
@@ -1,7 +1,7 @@
 module.exports = {
     mode: 'development',
 
-    entry: __dirname + '/src/index.jsx',
+    entry: __dirname + '/src/index.tsx',
 
     output: {
         path: __dirname + '/dist/',
